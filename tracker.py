beb8ef22e6ae19c27e94b00e8397ed524081b5b1
Akshit Kanaujia <72263417+imakshit2125@users.noreply.github.com>
2022-01-19 16:05:16 +0530

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -5,8 +5,8 @@ import time
 api_key = 'your_coinmarketcap_api_key'
 bot_token = 'your_telegram_bot_token'
 chat_id = 'your_telegram_account_chat_id_here'
-threshold = 30000
-time_interval = 5 * 60 # in seconds
+threshold = 30000 # If the price is below our threshold, out tracker will send us alert
+time_interval = 5 * 60 # Periodic time after which our tracker send us alert
 
 
 def get_btc_price():
